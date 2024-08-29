e149e62cdd91231d14319abea2b7fadbd4e51f76
neves-c-leonardo <neves.c.leonardo@gmail.com>
2024-08-29 19:57:40 -0300

    Adicionando arquivo operadores relacionais

--- operadores_relacionais.py
+++ operadores_relacionais.py
@@ -25,4 +25,54 @@ print(resultado)
 resultado = a <= b
 print(resultado)
 
-# Teste
+
+##  Ana está organizando uma festa do dia 26 e comprou os seguintes itens:
+# Pizzas: 8 unidades
+# Bebidas (em litros): 15 litros
+# Bolos: 3 unidades
+# Doces (em gramas): 500 gramas
+
+# As quantidades mínimas necessárias são:
+# Pizzas: no mínimo 10 unidades
+# Bebidas: no mínimo 12 litros
+# Bolos: no mínimo 5 unidades
+# Doces: no mínimo 600 gramas
+
+## Objetivo: Verificar se as quantidades compradas atendem ou 
+# excedem as necessidades mínimas e identificar quais itens 
+# estão abaixo da quantidade mínima.
+
+dia = int(input("Qual o dia de hoje? "))
+pedidoPizza = int(input("Quantas pizzas comprou? "))
+pedidoBebida = int(input("Quantas bebidaS comprou? "))
+pedidoBolo = int(input("Quantas bolos comprou? "))
+pedidoDoce = int(input("Quantas doces comprou? "))
+#  Declarando variaveis
+diaFesta = 26
+pedidoMinimoPizza = 10
+pedidoMinimoBebida = 12
+pedidoMinimoBolo = 5
+pedidoMinimoDoce = 600
+
+if diaFesta == dia:
+    print("Ana esta fazendo as compras no dia da festa!")
+else:
+    print("Ana esta fazendo compra adianta")
+
+if(pedidoMinimoPizza >= pedidoPizza):
+    if(pedidoMinimoPizza == pedidoPizza):
+        print("Ana comprou o minimo permitido de pizza")
+    print("Ana não comprou pizzas suficientes!")
+
+if(pedidoMinimoBebida < pedidoBebida):
+    print("Ana comprou mais bebidas que o necessário!")
+
+if(pedidoMinimoBolo < pedidoBolo):
+    print("Ana excedeu a compra de bolos!")
+elif(pedidoMinimoBolo == pedidoBolo):
+    print("Ana comprou o minimo permitido de bolo")
+else:
+    print("Ana não comprou bolos suficiente")
+
+if(pedidoMinimoDoce > pedidoDoce):
+    print("Ana não comprou doces suficiente")
