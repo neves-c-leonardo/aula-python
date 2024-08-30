8bb26f8bf245505e3567e60b853c51f0a469d0fc
neves-c-leonardo <neves.c.leonardo@gmail.com>
2024-08-29 21:44:09 -0300

    Operadores atributicao

---
+++ operadores_atribuicao.py
@@ -0,0 +1,17 @@
+x = 5
+
+x += 2
+print(x) # Resultado: 7
+
+x *= 2
+print(x) # Resultado: 14
+
+x -= 2
+print(x) # Resultado: 12
+
+x /= 2
+print(x) # Resultado: 6
+
+x = 12
+x //= 2
+print(x) # Resultado: 6
