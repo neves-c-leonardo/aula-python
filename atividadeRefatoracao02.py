2710312793592447cb8ca52a5da2f0d758ffdcbd
neves-c-leonardo <neves.c.leonardo@gmail.com>
2024-09-04 21:45:14 -0300

    Atualizando arquivo atividadeRefatoracao02

--- atividadeRefatoracao02.py
+++ atividadeRefatoracao02.py
@@ -1,9 +1,23 @@
-idade = 25
+# Variaveis Globais
+idade = int(input("Qual é a sua idade? "))
 
-categoria = "Indefinida"
-if idade < 13:
-    categoria = "Criança"
-elif idade >= 13 and idade < 18:
-    categoria = "Adolescente"
+# No trecho de condição será validado a idade da persona para analisar sua categproa
+def validarIdadePersona(idade):
+    mensagemPadrao = "A pessoa é classificada como: "
+    if idade < 0:
+        return "Essa pessoa por acaso ja nasceu?"
+    elif idade > 0 and idade <= 2:
+        return mensagemPadrao + "Bebê!"
+    elif idade > 2 and idade < 13:
+        return mensagemPadrao + "Criança!"
+    elif idade >= 13 and idade < 18:
+        return mensagemPadrao + "Adolescente!"
+    elif idade >= 18 and idade <= 60:
+        return mensagemPadrao + "Adulto!"
+    elif idade > 60 and idade <= 105:
+        return mensagemPadrao + "Idoso!"
+    else:
+        return "Alguem verifica se ainda ta vivo?!"
 
-print("A pessoa é classificada como: ", categoria)
+categoriaIdade = validarIdadePersona(idade) 
+print(categoriaIdade)
